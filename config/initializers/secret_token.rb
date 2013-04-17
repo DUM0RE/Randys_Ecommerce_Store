f8afa840461f344e4cf24e7d2dac8d603cba7402
Mitch Green <mitchgrn@gmail.com>
2013-04-16 19:22:38 -0500

    Basic App Shell

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandysEcommerceStore::Application.config.secret_token = '5da0304d6f8ab23e9a0a53465c124d5952ab43ec6fcebbf8c89e304a802229340bca084aa2c498974aa674e7e9c0448db75c7985c42fda2973407bc12ce2f455'
