f481b4ea25ebdffd24bf33f76c68e3a5067fa65a
Mitch Green <mitchgrn@gmail.com>
2013-04-16 21:47:24 -0500

    Added ActiveAdmin page, Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :platform_id, :price, :stock_quantity
+  attr_accessible :description, :name, :platform_id, :price, :stock_quantity, :image, :on_sale, :sale_price
   belongs_to :platform
   has_many :line_items
 end
